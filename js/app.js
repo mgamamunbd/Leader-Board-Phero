3593141ea42ac9c2557b04366aad74e5a8c1de47
mgamamunbd <mgamamunbd@gmail.com>
2022-02-15 16:43:06 +0400

    add button added

--- js/app.js
+++ js/app.js
@@ -6,8 +6,18 @@ topPlayers.style.color = "rgba(255,0,255,0.3)";
 //color for top blogs section
 var topBlogs = document.getElementById("top-blogs");
 topBlogs.style.color = "rgba(255,0,255,0.3)";
-//background color for player section
+//background color for player section by for of loop
 var playerSection = document.getElementsByClassName("players");
 for (const player of playerSection) {
   player.style.backgroundColor = "rgba(255,0,255,0.3)";
 }
+
+//list item adding by button
+const addListItem = document
+  .getElementById("add-button")
+  .addEventListener("click", function () {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = "React";
+    const list = document.getElementById("course");
+    list.appendChild(listItem);
+  });
