dfb299edbc06aa74d6489e7c0b9a4701dc4afdcf
mgamamunbd <mgamamunbd@gmail.com>
2022-02-15 14:41:24 +0400

    js file added

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/** @format */
+var topPlayers = document.getElementById("top-player");
+topPlayers.style.color = "rgba(255,0,255,0.3)";
+
+var topBlogs = document.getElementById("top-blogs");
+topBlogs.style.color = "rgba(255,0,255,0.3)";
