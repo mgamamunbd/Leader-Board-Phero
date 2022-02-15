e56e8b15724bbd30e4104685e40ccd4119afb9b8
mgamamunbd <mgamamunbd@gmail.com>
2022-02-15 14:48:30 +0400

    style added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 /** @format */
+
+//color for top player section
 var topPlayers = document.getElementById("top-player");
 topPlayers.style.color = "rgba(255,0,255,0.3)";
-
+//color for top blogs section
 var topBlogs = document.getElementById("top-blogs");
 topBlogs.style.color = "rgba(255,0,255,0.3)";
+//background color for player section
+var playerSection = document.getElementsByClassName("players");
+for (const player of playerSection) {
+  player.style.backgroundColor = "rgba(255,0,255,0.3)";
+}
